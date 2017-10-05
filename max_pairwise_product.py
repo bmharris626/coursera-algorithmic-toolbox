31ae1346b655298d4f06ef847700492cb686f7c5
Harris <Harris.Brandon@bls.gov>
2017-10-05 13:51:54 -0400

    Week 1 - Completed

--- max_pairwise_product.py
+++ max_pairwise_product.py
@@ -1,12 +1,47 @@
 # Uses python3
-n = int(input())
-a = [int(x) for x in input().split()]
-assert(len(a) == n)
+def MaxPairwiseProduct(a):
+    result = 0
+    x, y = 0, 0
+    for i in range(0, len(a) - 1):
+        for j in range(i+1, len(a)):
+            if a[i]*a[j] > result:
+                x, y = a[i], a[j]
+                result = a[i]*a[j]
+    return [(x,y), result]
 
-i, j = 0, 0
-for x in a:
-    if x > i:
-        j = i
-        i = x
+def MaxPairwiseProduct_fast(a):
+    i, j = 0, 0
+    for x in a:
+        if x > i:
+            j = i
+            i = x
+        elif x > j:
+            j = x
+    return [(i, j), i * j]
 
-print(i * j)
+def stress_test(mx, nx, ax):
+    i = 0
+    while True:
+        n = random.randint(0, nx) + 2
+        print(n)
+        a = [random.randint(0, ax) for i in range(n)]
+        print(a)
+        res1 = MaxPairwiseProduct(a)
+        res2 = MaxPairwiseProduct_fast(a)
+        if (res1[1] != res2[1]):
+            print("Wrong answer:", res1, res2)
+            break
+        elif i == mx: break;
+        else: print("OK")
+        i += 1
+
+def main():
+    n = int(input())
+    a = [int(x) for x in input().split()]
+    assert(len(a) == n)
+    result = MaxPairwiseProduct_fast(a)
+    print(result[1])
+
+if __name__ == '__main__':
+    # stress_test(1000, 100, 10000)
+    main()
