d0da44c5844ced920cf07455f9c2854358e3f22a
Harris <Harris.Brandon@bls.gov>
2017-10-11 08:48:25 -0400

    Updated largest_number.py

--- greedy-algorithms/02_greedy_algorithms_submitted_answers/largest_number.py
+++ greedy-algorithms/02_greedy_algorithms_submitted_answers/largest_number.py
@@ -4,23 +4,19 @@ import sys
 
 def IsGreaterOrEqual(digit, maxDigit):
     if maxDigit == -float('inf'): return True
-    if digit == maxDigit: return True
-    digit, maxDigit = str(digit), str(maxDigit)
-    n = min(len(digit), len(maxDigit))
-    if (digit[:n] > maxDigit[:n]): return True
-    if (digit[:n] == maxDigit[:n]) and (digit[:-n-1:-1] > maxDigit[:-n-1:-1]):
-        return True
-    return False
+    t1 = int(str(digit) + str(maxDigit))
+    t2 = int(str(maxDigit) + str(digit))
+    return t1 >= t2
 
-def largest_number(a):
-    res = str()
-    while len(a) > 0:
+def largest_number(Digits):
+    answer = str()
+    while len(Digits) > 0:
         maxDigit = -float('inf')
-        for n in a:
-            if IsGreaterOrEqual(n, maxDigit): maxDigit = n
-        res += str(maxDigit)
-        a.remove(maxDigit)
-    return res
+        for digit in Digits:
+            if IsGreaterOrEqual(digit, maxDigit): maxDigit = digit
+        answer += str(maxDigit)
+        Digits.remove(maxDigit)
+    return answer
 
 if __name__ == '__main__':
     input = sys.stdin.read()
