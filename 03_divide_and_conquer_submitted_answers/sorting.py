bee0deefc07fe139b846047af97bee0a2183e685
Harris <Harris.Brandon@bls.gov>
2017-10-16 14:06:03 -0400

    Finished #3 - 03 Divide & Conquer

--- 03_divide_and_conquer_submitted_answers/sorting.py
+++ 03_divide_and_conquer_submitted_answers/sorting.py
@@ -3,30 +3,29 @@ import sys
 import random
 
 def partition3(a, l, r):
-    #write your code here
-    pass
-
-def partition2(a, l, r):
     x = a[l]
-    j = l;
+    m1, m2 = l, l
     for i in range(l + 1, r + 1):
-        if a[i] <= x:
-            j += 1
-            a[i], a[j] = a[j], a[i]
-    a[l], a[j] = a[j], a[l]
-    return j
-
+        y = a[i]
+        if y <= x:
+            m1 += 1
+            a[i], a[m1] = a[m1], a[i]
+        if y == x:
+            m2 += 1
+            a[m1], a[m2] = a[m2], a[m1]
+    j = 0
+    for i in range(l, m2 + 1):
+        a[i], a[m1 - j] = a[m1 - j], a[i]
+        j += 1
+    return m1, m1 - j
 
 def randomized_quick_sort(a, l, r):
-    if l >= r:
-        return
+    if l >= r: return
     k = random.randint(l, r)
     a[l], a[k] = a[k], a[l]
-    #use partition3
-    m = partition2(a, l, r)
-    randomized_quick_sort(a, l, m - 1);
-    randomized_quick_sort(a, m + 1, r);
-
+    m1, m2 = partition3(a, l, r)
+    randomized_quick_sort(a, l, m2)
+    randomized_quick_sort(a, m1 + 1, r)
 
 if __name__ == '__main__':
     input = sys.stdin.read()
