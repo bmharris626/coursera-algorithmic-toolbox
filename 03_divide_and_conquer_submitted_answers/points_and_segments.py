f20db5558e0fd8f3c5056f5d90c0b38b21336100
Brandon Harris <bmharris626@gmail.com>
2017-12-23 19:44:55 -0500

    Completed ex5 of part3

--- 03_divide_and_conquer_submitted_answers/points_and_segments.py
+++ 03_divide_and_conquer_submitted_answers/points_and_segments.py
@@ -1,18 +1,61 @@
 # Uses python3
-import sys
+import sys, random
+
+def lte(a, b):
+    d = {'p': 1, 's': 0, 'e': 2}
+    if a == b: return True
+    if a[0] == b[0]: return d[a[1]] < d[b[1]]
+    return a[0] < b[0]
+
+def binary_search(A, key):
+    low, high = 0, len(A) - 1
+    while low <= high:
+        mid = int(low + (high - low) / 2)
+        if key == A[mid]: return mid
+        elif key < A[mid]: high = mid - 1
+        else: low = mid + 1
+    return -1
+
+def partition3(a, l, r):
+    x = a[l]
+    m1, m2 = l, l
+    for i in range(l + 1, r + 1):
+        y = a[i]
+        if lte(y, x):
+            m1 += 1
+            a[i], a[m1] = a[m1], a[i]
+        if y == x:
+            m2 += 1
+            a[m1], a[m2] = a[m2], a[m1]
+    j = 0
+    for i in range(l, m2 + 1):
+        a[i], a[m1 - j] = a[m1 - j], a[i]
+        j += 1
+    return m1, m1 - j
+
+def randomized_quick_sort(a, l, r):
+    if l >= r: return
+    k = random.randint(l, r)
+    a[l], a[k] = a[k], a[l]
+    m1, m2 = partition3(a, l, r)
+    randomized_quick_sort(a, l, m2)
+    randomized_quick_sort(a, m1 + 1, r)
 
 def fast_count_segments(starts, ends, points):
-    S = list(zip(starts, 's'*len(starts)))
-    E = list(zip(ends, 'e'*len(ends)))
-    P = list(zip(points, 'p'*len(points)))
-    A = S + E + P
-    A.sort(key=lambda x: x[0])
+    a = list(zip(starts, len(starts)*'s'))
+    a.extend(list(zip(ends, len(ends)*'e')))
+    a.extend(list(zip(points, len(points)*'p')))
+    randomized_quick_sort(a, 0, len(a) - 1)
+    n = len(ends)
+    s, e, = 0, n
+    p = dict()
+    for element, typ in a:
+        if typ == 'e': e -= 1
+        elif typ == 's': s += 1
+        else: p[element] = e + s - n
     cnt = list()
-    s = 0
-    for a in A:
-        if a[1] == 's': s += 1
-        if a[1] == 'e': s -= 1
-        if a[1] == 'p': cnt.append(s)
+    for pnt in points:
+        cnt.append(p[pnt])
     return cnt
 
 def naive_count_segments(starts, ends, points):
--- 03_divide_and_conquer_submitted_answers/sorting.py
+++ 03_divide_and_conquer_submitted_answers/sorting.py
@@ -3,6 +3,19 @@ import sys
 import random
 
 def partition3(a, l, r):
+    """
+    a => list of numbers being sorted
+    l => starting index of the list
+    r => the last index of the list
+
+    Checks the list from index (l + 1) to r for values that are less than or 
+    equal to a[l]. If the value is equal to or less than a[1] it is moved to 
+    the right of a[l] represented by the index m1. If the value is equal
+    a[l] then the value is moved from poition m1 to the beginning of the
+    values to the left of a[l] represented by m2. The function then returns
+    the values m1, and (m1 -j) representing the two halves of the list that
+    are still unsorted. (List1 = a[l:(m2-j)], List2[m1:r])
+    """
     x = a[l]
     m1, m2 = l, l
     for i in range(l + 1, r + 1):
@@ -20,6 +33,19 @@ def partition3(a, l, r):
     return m1, m1 - j
 
 def randomized_quick_sort(a, l, r):
+    """
+    a => list of numbers to be sorted
+    l => starting index of the list
+    r => the last index of the list
+
+    First checks if the first index(l) is greater or equal to index(r), if True
+    then a must have only 1 element and so the list is just returned. If False
+    then the function selects a random value from the list and switches the
+    value with the first element of the list. Then partion3 is called sort the
+    list into values smaller than the value and larger then the value,
+    represented by m1, and m2. The function then makes a recursive call to
+    itself with new lists a[(m1+1):r], and a[l:m2].
+    """
     if l >= r: return
     k = random.randint(l, r)
     a[l], a[k] = a[k], a[l]
