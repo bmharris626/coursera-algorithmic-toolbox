d3be64bcab6a5f74b897be1f3144044d1dbe5601
Harris <Harris.Brandon@bls.gov>
2018-01-08 16:22:28 -0500

    04-Ex2 MemoryExceeded

--- 04_dynamic_programming_submitted_answers/knapsack.py
+++ 04_dynamic_programming_submitted_answers/knapsack.py
@@ -2,12 +2,18 @@
 import sys
 
 def optimal_weight(W, w):
-    # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+    w.insert(0, 0)
+    value = dict()
+    n = len(w)
+    for i in range(n + 1):
+        for wt in range(W + 1):
+            if i == 0 or wt == 0: value[(wt, i)] = 0
+            else:
+                value[(wt, i)] = value[(wt, i - 1)]
+                if w[i] <= wt:
+                    val = value[(wt-w[i], i-1)] + w[i]
+                    if val > value[(wt, i)]: value[(wt, i)] = val
+    return value[(W, n)]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
