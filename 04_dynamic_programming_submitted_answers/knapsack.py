6650d396ec388df8d07ff5125ae034992f16fd17
Harris <Harris.Brandon@bls.gov>
2018-01-10 14:32:54 -0500

    Completed 05-Ex2

--- 04_dynamic_programming_submitted_answers/knapsack.py
+++ 04_dynamic_programming_submitted_answers/knapsack.py
@@ -3,17 +3,17 @@ import sys
 
 def optimal_weight(W, w):
     w.insert(0, 0)
-    value = dict()
-    n = len(w)
-    for i in range(n + 1):
-        for wt in range(W + 1):
-            if i == 0 or wt == 0: value[(wt, i)] = 0
+    value = [list() for i in range(W + 1)]
+    i = 0
+    for wt in range(W + 1):
+        for i, wi in enumerate(w):
+            if i == 0 or wt == 0: value[wt].append(0)
             else:
-                value[(wt, i)] = value[(wt, i - 1)]
-                if w[i] <= wt:
-                    val = value[(wt-w[i], i-1)] + w[i]
-                    if val > value[(wt, i)]: value[(wt, i)] = val
-    return value[(W, n)]
+                value[wt].append(value[wt][i-1])
+                if wi <= wt:
+                    val = value[wt-wi][i-1] + wi
+                    if val > value[wt][i]: value[wt][i] = val
+    return value[W][i]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
